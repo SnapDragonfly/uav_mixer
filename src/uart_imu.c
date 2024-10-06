5d02b1f6cebc5d7fe8853fb8151923a705f1accb
Daniel Li <lida_mail@163.com>
2024-10-06 22:37:47 +0100

    fix uart can't be opened sometime

--- src/uart_imu.c
+++ src/uart_imu.c
@@ -23,6 +23,23 @@ int initialize_uart(const char* device, int baudrate) {
 
     struct termios options;
     tcgetattr(uart_fd, &options);
+    options.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+    options.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+    options.c_cflag &= ~CSIZE; // Clear all bits that set the data size
+    options.c_cflag |= CS8; // 8 bits per byte (most common)
+    options.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+    options.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+    options.c_lflag &= ~ICANON;
+    options.c_lflag &= ~ECHO; // Disable echo
+    options.c_lflag &= ~ECHOE; // Disable erasure
+    options.c_lflag &= ~ECHONL; // Disable new-line echo
+    options.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+    options.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+    options.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+    options.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+    options.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+    options.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+    options.c_cc[VMIN] = 0;
     switch(baudrate) {
         case 9600: cfsetispeed(&options, B9600); cfsetospeed(&options, B9600); break;
         case 19200: cfsetispeed(&options, B19200); cfsetospeed(&options, B19200); break;
@@ -31,12 +48,6 @@ int initialize_uart(const char* device, int baudrate) {
         // Add more cases as needed
         default: printf("Unsupported baud rate: %d", baudrate); return -2;
     }
-    options.c_cflag |= (CLOCAL | CREAD);
-    options.c_cflag &= ~CSIZE;
-    options.c_cflag |= CS8;
-    options.c_cflag &= ~PARENB;
-    options.c_cflag &= ~CSTOPB;
-    options.c_cflag &= ~CRTSCTS;
     tcsetattr(uart_fd, TCSANOW, &options);
 
     return uart_fd;
